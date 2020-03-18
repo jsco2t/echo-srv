d2154a969c94ce267bce98af9538ba0fd53672e9
Jason Scott <jsco2t@outlook.com>
2020-03-18 11:50:23 -0700

    Reporting port and making https optional

--- src/server/server.js
+++ src/server/server.js
@@ -6,26 +6,38 @@ var httpsPort = 443;
 var fileSys = require('fs');
 var space = '    ';
 var echo = require('./echo.js');
-
-const sslOpts = {
-    key: fileSys.readFileSync(process.env.CERT_PATH + 'privatekey.pem'),
-    cert: fileSys.readFileSync(process.env.CERT_PATH + 'fullchain.pem'),
-};
+var sslKeyPath = process.env.CERT_PATH + 'privatekey.pem';
+var sslCertPath = process.env.CERT_PATH + 'fullchain.pem';
 
 http.createServer(serverHandler).listen(
     80,
     ()=> console.log("HTTP ready")
 );
 
-https.createServer(sslOpts, serverHandler).listen(
-    httpsPort,
-    ()=> console.log("HTTPS ready")
-);
+if (fileSys.existsSync(sslKeyPath) && fileSys.existsSync(sslCertPath)) {
+  console.log("SSL cert and key files exist, setting up HTTPS support");
+
+  const sslOpts = {
+    key: fileSys.readFileSync(sslKeyPath),
+    cert: fileSys.readFileSync(sslCertPath),
+  };
+
+  https.createServer(sslOpts, serverHandler).listen(
+      httpsPort,
+      ()=> console.log("HTTPS ready")
+  );
+} else {
+  console.log("SSL cert or key files DO NOT exist - HTTPS support is not enabled");
+  console.log(`Search path for SSL Key: ${sslKeyPath}`);
+  console.log(`Search path for SSL Cert: ${sslCertPath}`);
+  console.log("File path for key and cert can be set with environmental variable: CERT_PATH")
+}
+
 
 function serverHandler(req, res) {
     req.on('data', ()=> null);
     req.on('end', ()=> {
-        
+
         res.sendDate = true;
 
         // use echo object to send output
